9e4fb7f45b4d88cfb54f08bafff6c7c0a0d9513f
ocean0801 <87162635+ocean0801@users.noreply.github.com>
2021-10-12 10:51:04 +0900

    [Test]tests.py
    Actionsにあわせちょっと改良。

--- rconc/tests.py
+++ rconc/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class DjangoMinecraftViewTests(TestCase):
+    def test_script(self):
+		pass
