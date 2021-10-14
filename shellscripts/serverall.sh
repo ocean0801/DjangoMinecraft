e32fc80dca2a8fdfdcc2edef91e52d938868a452
ocean0801 <87162635+ocean0801@users.noreply.github.com>
2021-10-14 20:22:16 +0900

    []]

--- shellscripts/serverall.sh
+++
@@ -1,5 +0,0 @@
-cd ../
-python3 manage.py makemigrations
-python3 manage.py migrate
-cd ../../server
-python3 ../DjangoMinecraft/manage.py runserver & python3 ../DjangoMinecraft/code1.py & python3 ../DjangoMinecraft/code2.py & java -jar -Xmx4G server.jar nogui
